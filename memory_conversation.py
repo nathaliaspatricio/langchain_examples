ad5288ca82e2c95a2bbef7785938caf26ce03d6a
Nathalia Sautchuk Patrício <nathalia.sautchuk@gmail.com>
2023-04-22 00:25:09 +0200

    Exemplo simples de conversa com memória

---
+++ memory_conversation.py
@@ -0,0 +1,30 @@
+##Código Teste sobre o LangChain
+##Tutorial completo em: https://www.youtube.com/watch?v=X550Zbz_ROE
+
+##A OPENAI_API_KEY deve ser gerado na sua conta no OpenAI: https://platform.openai.com/account/api-keys
+import os
+os.environ["OPENAI_API_KEY"] = ""
+
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain import OpenAI
+from langchain.chains import ConversationChain
+
+llm = OpenAI(
+    model_name = "text-davinci-003", 
+    temperature = 0, 
+    max_tokens = 256
+) 
+
+memory = ConversationBufferMemory()
+
+conversation = ConversationChain(
+    llm=llm,
+    verbose=True,
+    memory=memory
+)
+
+conversation.predict(input="Oi! Eu sou a Maria.")
+conversation.predict(input="Eu gosto de ler livros e você?")
+conversation.predict(input="Eu adoro os livros do Harry Potter.")
+
+print(conversation.memory.buffer)
